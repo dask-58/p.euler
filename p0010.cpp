464fe896f17f29f58aff90f78c96c2298e55aa99
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-04-24 17:28:11 +0530

    Refactor prime number calculation by removing sieve function and implementing direct accumulation

--- p0010.cpp
+++ p0010.cpp
@@ -1,28 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int64_t> sieve(int64_t n) {
+int main (int argc, char *argv[]) {
   vector<int64_t> primes;
-  vector<bool> is_prime(n + 1, true);
-  is_prime[0] = is_prime[1] = false;
-  for (int i = 2; 1LL * i * i <= n; i++) {
-    if (is_prime[i]) {
-      for (int j = 1LL * i * i; j <= n; j += i) {
-        is_prime[j] = false;
+  int64_t n = 2;
+  int64_t ans = 0;
+  while (primes.size() < 2'000'000) {
+    bool is_prime = true;
+    for (auto& num : primes) {
+      if (1LL * num * num > n) break;
+      if (n % num == 0) {
+        is_prime = false;
+        break;
       }
     }
-  }
-  for (int i = 2; i <= n; i++) {
-    if (is_prime[i]) {
-      primes.push_back(i);
+    if (is_prime) {
+      ans += n;
     }
+    n++;
   }
-  return primes;
-}
-
-int main (int argc, char *argv[]) {
-  vector<int64_t> primes = sieve(2'000'000);
-  int ans = accumulate(primes.begin(), primes.end(), 0LL);
   cout << ans;
   return 0;
 }
